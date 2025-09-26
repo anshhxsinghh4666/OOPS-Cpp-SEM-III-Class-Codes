5e0788e895d1eb5379643a478111afc9641c8fc6
Ansh Kumar Singh <anshkr.win8@gmail.com>
2025-09-26 23:17:52 +0530

    Perfect Number

--- 39_Perfect_Numbers.cpp
+++ 39_Perfect_Numbers.cpp
@@ -9,7 +9,105 @@ PERFECT NUMBERS :
 
 */
 
-
+#include <cstring>
 #include <iostream>
 using namespace std;
 
+void prime(int n) {
+    int isPrime = 1;
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+    if (isPrime == 0) {
+        cout << "Not a Prime Number\n";
+    } else {
+        cout << "Is a Prime Number\n";
+    }
+}
+
+void Armstrong() {
+    char n[100];
+    cout << "Enter Number : ";
+    cin >> n;
+
+    int count = 0, c = 0;
+    int len = strlen(n);
+
+    for (int i = 0; i < len; i++) {
+        if (isdigit(n[i])) {
+            int digit = n[i] - '0';
+            count += pow(digit, len);
+            c = c * 10 + digit;
+        }
+    }
+    if (c == count) {
+        cout << "Armstrong Number" << endl;
+    } else {
+        cout << "Not An Armstrong Number" << endl;
+    }
+}
+
+void Palindrome() {
+    char num[100];
+    cout << "Enter Number : ";
+    cin >> num;
+    int n = strlen(num);
+    bool isPalindrome = true;
+
+    for (int i = 0; i < n / 2; i++) {
+        if (num[i] != num[n - i - 1]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    if (isPalindrome) {
+        cout << num << " Is a Palindrome " << endl;
+    } else {
+        cout << num << " Is Not a Palindrome" << endl;
+    }
+}
+
+void Fibonacci() {
+    int n;
+    cout << "Enter Number of Elements : ";
+    cin >> n;
+    if (n < 0) {
+        cout << "Invalid Input";
+        return;
+    }
+
+    int t1 = 0, t2 = 1, nextTerm;
+
+    for (int i = 0; i < n; i++) {
+        cout << t1 << "\t";
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    cout << endl;
+}
+
+void factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    cout << "Factorial of " << n << " = " << fact << endl;
+}
+
+int main() {
+    prime(11);
+
+    Armstrong();
+
+    Palindrome();
+
+    Fibonacci();
+
+    factorial(10);
+
+    return 0;
+}
